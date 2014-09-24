5b21538ca98dbc0ba4fd3a45237e2e17a8d709a1
jasonalmaturner <jasonalmaturner@gmail.com>
2014-09-24 17:16:14 -0600

    Need to do some styling

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,20 +3,44 @@ var app = angular.module('twitterClone');
 app.controller('mainCtrl', function($scope, parseService){
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
+  //The getParseData function will call the getData method on the parseService object.
+  //You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
 
+  $scope.getParseData = function(){
+    parseService.getData().then(function(data){
+      for(var i = 0; i < data.data.results.length; i++){
+        data.data.results[i].createdAt = new Date(data.data.results[i].createdAt)
+      }
+      $scope.messages = data.data.results;
+      // $scope.messages = $scope.messages.map(function(){
+      //   $scope.messages[].createdAt
+      // })
+      console.log($scope.messages[0].createdAt)
+      // console.log(data.data.results[0].createdAt) 
+      // console.log(data);
+    });
+  };
 
 
-  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+  //The postData function will take whatever the user typed in (hint: look at the html
+  //and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
 
+  $scope.postData = function(){
+    var messages = {};
+    messages.text = $scope.message;
+    parseService.postDatas(messages);
+    $scope.message = "";
+  };
 
-
-
+  $scope.sort = function(){
+    
+  }
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1000)
+
+  setInterval(function(){
+    $scope.getParseData();
+  }, 1000)
 })
--- js/parseService.js
+++ js/parseService.js
@@ -6,19 +6,34 @@ app.service('parseService', function($http, $q){
 
   //On the line below create the postData method. This method will add data to the parse backend.
   //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
-  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+  //Also, remember that $http returns a promise. So if you return the whole $http call
+  //(return $http(...)), you can then use .then in your controller.
   
   //postData method here
 
+  this.postDatas = function(stuff){
+    return $http({
+      method: 'POST',
+      data: stuff,
+      url: 'https://api.parse.com/1/classes/chat'
+    }).then(function(data){
+      deferred.resolve(data.data.results)
+    });
+  };
 
   //On the line below create a getData method. This method will retrieve data from the parse backend.
   //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
-  //Make sure that in your post request, the format of the data you sent to parse is an object with a key of text and a value of whatever the message was. IE {text: message}
+  //Make sure that in your post request, the format of the data you sent to parse is an object with
+  //a key of text and a value of whatever the message was. 
+  //IE {text: message}
   /*Also, we want to be able to manipulate the data we get back from parse before we pass it back to our controller. The problem is 
   when we're making a request, we don't know when the response will come back. This sound like a perfect use case for a promise. We can 
   create a promise that resolves with the correct data from parse whenever our $http get requests finishes. 
   /*
-  //Because of the way parse sends back their data, let's resolve our promise with data.data.results if you had the original parameter be date like below.
+  //Because of the way parse sends back their d.then(function(data){
+      deferred.resolve(data.data.results)
+    });ata, let's resolve our promise with data.data.results
+  //if you had the original parameter be date like below.
   /* 
     .then(function(data){
       deferred.resolve(data.data.results)
@@ -26,4 +41,12 @@ app.service('parseService', function($http, $q){
   */
 
   //getData method here
+
+  this.getData = function(){
+    return $http({
+      method: 'GET',
+      url: 'https://api.parse.com/1/classes/chat?order=-createdAt'
+    })
+  };
+
 });
